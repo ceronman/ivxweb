d72f9830ef63aab09be542ded573bc4535a1902a
Manuel Ceron <ceronman@gmail.com>
2010-12-21 20:35:23 +0100

    Addded missing keys method to object in util.

--- util.js
+++ util.js
@@ -25,3 +25,12 @@ var inherit = (function () {
 var cosole = console || {
     log: function (){}
 }
+
+Object.keys = Object.keys || function(o) {
+    var result = [];
+    for(var name in o) {
+        if (o.hasOwnProperty(name))
+          result.push(name);
+    }
+    return result;
+};
